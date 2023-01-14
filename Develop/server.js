596ec404d7ed069f3cc61f2a2fdd4990b2843069
TRK41 <Troyk@bell.net>
2023-01-14 18:56:28 -0500

    Building server.js code

---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const noteData = require('./db/notes.json');
+const PORT = 3001;
+const path = require('path');
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+
+  app.get('/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, 'public/notes.html'));
+    });
+
+  app.get('/api/notes', (req, res) => res.json(noteData));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
